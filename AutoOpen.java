b0d6f22715a999670b7c1b835db5c928965a0235
Crafthai <42124450+Crafthai@users.noreply.github.com>
2018-09-23 21:41:10 -0400

    added gold cards

--- AutoOpen.java
+++ AutoOpen.java
@@ -5,9 +5,13 @@ public class AutoOpen {
     public static void main(String[] args) {
 
         double numCommon = 0;
+        double goldCommon = 0;
         double numRare = 0;
+        double goldRare = 0;
         double numEpic = 0;
+        double goldEpic = 0;
         double numLegendary = 0;
+        double goldLegendary = 0;
         int legendaryIn;
         int threeLegendaryInPacks = 0;
         int fourLegendaryInPacks = 0;
@@ -17,6 +21,7 @@ public class AutoOpen {
         int legendary = 0;
         int legendaryTimer = 0;
         int number = 0;
+        int gold = 0;
         char goAgain = 'y';
         Random card = new Random();
 
@@ -31,6 +36,7 @@ public class AutoOpen {
             for (int j = 0; j < packs; j++) {
                 legendaryIn = 0;
                 for (int i = 0; i < 5; i++) {
+
                     if (legendary == 195) {
                         number = 100;
                         legendaryTimer++;
@@ -45,22 +51,50 @@ public class AutoOpen {
                     }
 
                     if (number < 270) {
-                        System.out.println("common");
+                        gold = card.nextInt(47);
+
+                        if(gold == 23) {
+                            System.out.println("golden common");
+                            goldCommon++;
+                        } else {
+                            System.out.println("common");
+                        }
                         numCommon++;
                         legendary++;
                         epic++;
                     } else if (number > 269 && number < 294) {
-                        System.out.println("rare!");
+                        gold = card.nextInt(16);
+
+                        if(gold == 8) {
+                            System.out.println("golden rare!");
+                            goldRare++;
+                        } else {
+                            System.out.println("rare!");
+                        }
                         numRare++;
                         legendary++;
                         epic++;
                     } else if (number > 293 && number < 299) {
-                        System.out.println("EPIC!");
+                        gold = card.nextInt(16);
+
+                        if(gold == 8) {
+                            System.out.println("GOLDEN EPIC!");
+                            goldEpic++;
+                        } else {
+                            System.out.println("EPIC!");
+                        }
                         numEpic++;
                         legendary++;
                         epic = 0;
                     } else {
-                        System.out.println("L E G E N D A R Y ! ! !");
+                        gold = card.nextInt(11);
+
+                        if(gold == 8) {
+                            System.out.println("WAAAAHHHHH   G O L D E N   L E G E N D A R Y ! ! !");
+                            goldLegendary++;
+                        } else {
+                            System.out.println("L E G E N D A R Y!!!!");
+                        }
                         numLegendary++;
                         legendaryIn++;
                         legendary = 0;
@@ -92,6 +126,10 @@ public class AutoOpen {
                     "\npercentage of legendaries: " + ((numLegendary / cards) * 100) + "%" +
                     "\nnumber of epics hit by pity timer: " + epicTimer +
                     "\nnumber of legendaries hit by pity timer: " + legendaryTimer);
+            System.out.println("\npercentage of gold commons: " + ((goldCommon / numCommon) * 100) + "%" +
+                    "\npercentage of gold rares: " + ((goldRare / numRare) * 100) + "%" +
+                    "\npercentage of gold epics: " + ((goldEpic / numEpic) * 100) + "%" +
+                    "\npercentage of golden legendaries: " + ((goldLegendary / numLegendary) * 100) + "%");
             System.out.println("\nnumber of times with 3 legendaries in a pack: " + threeLegendaryInPacks +
                     "\nnumber of times with 4 legendaries in a pack: " + fourLegendaryInPacks +
                     "\nnumber of times with 5 legendaries in a pack: " + fiveLegendaryInPacks);
